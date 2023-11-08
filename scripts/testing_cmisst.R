518c0d9a5041b693fece1c24c8ad975d9dd004ba
Brian Burke <brian.burke@noaa.gov>
2023-11-07 16:41:18 -0800

    Aesthetics in the code

--- scripts/testing_cmisst.R
+++ scripts/testing_cmisst.R
@@ -5,6 +5,17 @@ library(sf)
 library(ggplot2)
 library(RColorBrewer)
 
+source('scripts/get_index.R')
+
+# get the land for plotting (wrap across antimeridian)
+land<-rnaturalearth::ne_countries(type='countries', scale = "large", returnclass = "sf")
+amer <- land[land$region_un=='Americas',]
+# shift the Americas to a more Pacific centric worldview
+pacified_amer <- st_shift_longitude(amer)
+rest_of_world <- land[!land$region_un=='Americas',]
+land2 <- rbind(pacified_amer, rest_of_world)
+
+# set parameters
 dataSet='ERSST'
 min.lon=158
 max.lon=246
@@ -12,26 +23,18 @@ min.lat=10
 max.lat=62
 years=seq(1981, 2020, 1)
 months=seq(1,12,1)
-response <- data.frame(year=years, val=runif(length(years)))
 removeBering=TRUE
 returnDataType='anom'
 returnObjectType='array'
 
-
-# get the land in order
-land<-rnaturalearth::ne_countries(type='countries', scale = "large", returnclass = "sf")
-amer <- land[land$region_un=='Americas',]
-# shift the Americas to a more Pacific centric worldview
-pacified_amer <- st_shift_longitude(amer)
-rest_of_world <- land[!land$region_un=='Americas',]
-# this is the important section: bind together the shifted and unshifted parts
-land2 <- rbind(pacified_amer, rest_of_world)
-
-
-source('scripts/get_index.R')
+response <- data.frame(year=years, val=runif(length(years)))
 
 cmisst <- get_CMISST_index(response = response, dataSet = dataSet)
 
+index <- cmisst[[1]]
+plot(index$year, index$spr.cov, type='b', xlab="", ylab="CMISST Index")
+
+# What to plot
 covMap<-cmisst[[2]] # wint
 covMap<-cmisst[[3]] # spr
 covMap<-cmisst[[4]] # sum
@@ -42,9 +45,10 @@ myPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")), space="Lab")
 ggplot() +
   geom_raster(data = melt(covMap),
               aes(x = Var1, y = Var2, fill=value)) +
-  geom_sf(data=land2, color="black", fill="wheat", linewidth=0.25) +
+  geom_sf(data=land2, color="black", fill="grey", linewidth=0.25) +
   xlim(min.lon, max.lon) + ylim(min.lat, max.lat) +
   #scale_fill_gradientn(colours = myPalette(100),limits=c(-0.6,0.5),name="Covariance", na.value = "white") +
   scale_fill_gradientn(colours = myPalette(100), name="Covariance", na.value = "white") +
+  theme_classic() + theme(panel.border = element_rect(colour = "grey", fill=NA)) +
   labs(x = "Longitude", y = "Latitude")
 
